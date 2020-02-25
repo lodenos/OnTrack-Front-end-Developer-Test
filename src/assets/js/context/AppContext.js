31a2c04df7f1e7faf1a64b0dcaad78f05e8e0c29
lodenos <nikolaiilodenos@gmail.com>
2020-02-25 08:47:26 +0000

    commit the technical test

---
+++ src/assets/js/context/AppContext.js
@@ -0,0 +1,14 @@
+import React from 'React';
+
+export default React.createContext({
+  books: {},
+  count: 0,
+  filters: [],
+  index: 1,
+  indexMax: 1,
+  searchFilters: () => {},
+  textInput: '',
+  updateBooks: () => {},
+  updateIndex: () => {},
+  waiting: true,
+});
